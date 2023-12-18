d9c55601aa4cb3b3459f8585f18b476c1fc0c7b6
Mark <mark.langezaal@gmail.com>
2023-12-18 19:15:59 +0100

    Update appversion.ts

--- src/appversion.ts
+++ src/appversion.ts
@@ -1,6 +1,6 @@
 export class AppVersion
 {
-    static version = '0.33.1';
+    static version = '0.33.2';
     static name = 'SudokuPad Penpa+ Importer';
     static getAppTitle() {    
         return `${AppVersion.name} v${AppVersion.version}`;
