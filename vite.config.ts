03532dfbcb6659019b3f24f2a171adab5d2a0a8d
Mark <mark.langezaal@gmail.com>
2024-01-08 04:02:24 +0100

    Refactor build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
+import { fileURLToPath, URL } from 'node:url';
+
 import { defineConfig } from 'vite';
+// import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [],
-	build: {
-	//  outDir: '../dist'
+	// plugins: [vue()],
+	resolve: {
+		alias: {
+			'@': fileURLToPath(new URL('./src', import.meta.url)),
+		},
 	},
+	// hosted at: https://marktekfan.github.io/sudokupad-penpa-import/
 	base: '/sudokupad-penpa-import/',
 });
